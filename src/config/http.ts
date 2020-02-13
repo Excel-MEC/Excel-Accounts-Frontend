4686e8f3567c67d9262c26e207196da9a7273b69
AjeshKumarS <ajeshkumar36020@gmail.com>
2020-02-13 23:11:16 +0530

    Added Basic Authentication

--- src/config/http.ts
+++ src/config/http.ts
@@ -1,16 +1,36 @@
-export const post = (url: string, data: any) => {
-  return fetch(url, {
+import { ApiRoot } from './api';
+
+const post = (url: string, data: any) => {
+  const headers: { [k: string]: string } = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  };
+  if (localStorage.getItem('jwt_token')) {
+    headers.Authorization = 'Bearer ' + localStorage.getItem('jwt_token');
+  }
+  return fetch(ApiRoot + url, {
     method: 'POST',
-    body: data
+    headers: headers,
+    body: JSON.stringify(data)
   })
     .then(res => res.json())
     .catch(err => err);
 };
 
-export const get = (url: string) => {
-  return fetch(url, {
-    credentials: 'include'
+const get = (url: string) => {
+  const headers: { [k: string]: string } = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  };
+  if (localStorage.getItem('jwt_token')) {
+    headers.Authorization = 'Bearer ' + localStorage.getItem('jwt_token');
+  }
+  return fetch(ApiRoot + url, {
+    method: 'GET',
+    headers: headers
   })
     .then(res => res.json())
     .catch(err => console.log(err));
 };
+
+export default { post, get };

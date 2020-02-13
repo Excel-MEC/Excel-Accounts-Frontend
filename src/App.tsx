5421da13746f4c81d567010a755a23b4815643fa
AjeshKumarS <ajeshkumar36020@gmail.com>
2020-02-14 00:24:49 +0530

    Setup iframe in frontend for crossorigin authentication

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.scss';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Logout from './pages/Logout/Logout';
+import Authorize from './pages/Authorize/Authorize';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route path="/login" component={Login} />
           <Route path="/logout" component={Logout} />
+          <Route path="/authorize" component={Authorize} />
         </Switch>
       </div>
     </Router>
